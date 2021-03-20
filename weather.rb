76d0348ac9e732b530ca07f511d11b0772b7cfcc
shanecolby <shanecolbyzero8@gmail.com>
2021-03-20 08:43:11 -0500

    dynamic weather for user input city

--- weather.rb
+++ weather.rb
@@ -1,9 +1,17 @@
 require "http"
 
-weather = HTTP.get('http://api.openweathermap.org/data/2.5/weather?q=chicago&appid=APIKEY&units=imperial').parse
+p "Input a city to view current weather."
+userCity = gets.chomp
 
-temp = weather['main']['temp']
+
+weather = HTTP.get("http://api.openweathermap.org/data/2.5/weather?q=#{userCity}&appid=APIKEY&units=imperial").parse
+
+temperature = weather['main']['temp']
+
 current_conditions = weather['weather'][0]['description']
+
 city = weather['name']
 
-p "It is #{temp} degrees and #{current_conditions} in #{city}."
+
+
+p "It is #{temperature} degrees and #{current_conditions} in #{city}."
