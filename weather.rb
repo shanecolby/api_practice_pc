9a269d43d65588d7c146c3af661095ba091953f7
shanecolby <shanecolbyzero8@gmail.com>
2021-03-20 14:12:59 -0500

    dig deeper into api to pull out more specific data

--- weather.rb
+++ weather.rb
@@ -1,10 +1,10 @@
 require "http"
 
-p "Input a city to view current weather."
+# p "Input a city to view current weather."
 userCity = gets.chomp
 
 
-weather = HTTP.get("http://api.openweathermap.org/data/2.5/weather?q=#{userCity}&appid=APIKEY&units=imperial").parse
+weather = HTTP.get("http://api.openweathermap.org/data/2.5/weather?q=#{userCity}&appid=9b47e03139e2b9f69fb428cf5bee786c&units=imperial").parse
 
 temperature = weather['main']['temp']
 
@@ -12,6 +12,15 @@ current_conditions = weather['weather'][0]['description']
 
 city = weather['name']
 
+feels =  weather['main']['feels_like']
+
+high =  weather['main']['temp_max']
+
+low =  weather['main']['temp_min']
+
+wind_speed =  weather['wind']['speed']
+
+# p feels
 
 
-p "It is #{temperature} degrees and #{current_conditions} in #{city}."
+p "Hello, it is #{temperature} degrees and currently #{current_conditions}. It feels like #{feels}. Today's high is #{high} degrees and the low is #{low} degrees with a wind speed of #{wind_speed}. That is what to expect here in #{city}."
