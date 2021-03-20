94d5c6e37036bcb4e977d59a12a74e8057190fa0
shanecolby <shanecolbyzero8@gmail.com>
2021-03-20 08:27:26 -0500

    parse through data and pull out temp, conditions and city

--- weather.rb
+++ weather.rb
@@ -2,4 +2,8 @@ require "http"
 
 weather = HTTP.get('http://api.openweathermap.org/data/2.5/weather?q=chicago&appid=APIKEY&units=imperial').parse
 
-p weather
+temp = weather['main']['temp']
+current_conditions = weather['weather'][0]['description']
+city = weather['name']
+
+p "It is #{temp} degrees and #{current_conditions} in #{city}."
