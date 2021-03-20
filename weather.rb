1640c82cbec9071d0cab9c7eb415013b12b46f53
shanecolby <shanecolbyzero8@gmail.com>
2021-03-20 15:30:23 -0500

    fix

--- weather.rb
+++ weather.rb
@@ -1,10 +1,11 @@
 require "http"
 
-# p "Input a city to view current weather."
+p "Input a city to view current weather."
 userCity = gets.chomp
 
 
 weather = HTTP.get("http://api.openweathermap.org/data/2.5/weather?q=#{userCity}&appid=9b47e03139e2b9f69fb428cf5bee786c&units=imperial").parse
+
 
 temperature = weather['main']['temp']
 
@@ -20,7 +21,8 @@ low =  weather['main']['temp_min']
 
 wind_speed =  weather['wind']['speed']
 
-# p feels
+p feels
 
 
 p "Hello, it is #{temperature} degrees and currently #{current_conditions}. It feels like #{feels}. Today's high is #{high} degrees and the low is #{low} degrees with a wind speed of #{wind_speed}. That is what to expect here in #{city}."
+
